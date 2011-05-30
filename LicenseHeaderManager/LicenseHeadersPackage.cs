1153c03af261cd81695ec58d66ade8488cf8772e
Fabian Schmied <fabian.schmied@rubicon.eu>
2011-05-30 09:35:51 +0200

    Set version number down to 0.9 for internal beta test

--- LicenseHeaderManager/LicenseHeadersPackage.cs
+++ LicenseHeaderManager/LicenseHeadersPackage.cs
@@ -33,7 +33,7 @@ namespace LicenseHeaderManager
   [PackageRegistration (UseManagedResourcesOnly = true)]
   // This attribute is used to register the informations needed to show the this package
   // in the Help/About dialog of Visual Studio.
-  [InstalledProductRegistration ("#110", "#112", "1.0", IconResourceID = 400)]
+  [InstalledProductRegistration ("#110", "#112", "0.9", IconResourceID = 400)]
   // This attribute is needed to let the shell know that this package exposes some menus.
   [ProvideMenuResource ("Menus.ctmenu", 1)]
   [ProvideOptionPage (typeof (OptionsPage), "License Headers", "General", 0, 0, true)]
