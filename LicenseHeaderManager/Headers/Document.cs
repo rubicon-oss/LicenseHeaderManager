6b868b53c3cb78e11e652c535452a2c80731fc78
Daniel Sklenitzka <daniel.sklenitzka@w0297.int.rubicon-it.com>
2011-06-06 14:54:54 +0200

    Insertion of empty lines only when necessary (when a file starts with a comment)

--- LicenseHeaderManager/Headers/Document.cs
+++ LicenseHeaderManager/Headers/Document.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using EnvDTE;
+using Language = LicenseHeaderManager.Options.Language;
 
 namespace LicenseHeaderManager.Headers
 {
@@ -13,29 +14,33 @@ namespace LicenseHeaderManager.Headers
     private readonly TextDocument _document;
     private readonly Parser _parser;
 
-    public Document (TextDocument document, Options.Language language, string[] header, IEnumerable<string> keywords = null)
+    public Document (TextDocument document, Language language, string[] header, IEnumerable<string> keywords = null)
     {
       _document = document;
       if (header == null)
         _header = null;
       else
-        _header = string.Join(Environment.NewLine, header);
+        _header = string.Join (Environment.NewLine, header);
       _keywords = keywords;
 
       _parser = new Parser (language.LineComment, language.BeginComment, language.EndComment, language.BeginRegion, language.EndRegion);
     }
 
-    private string GetText ()
+    private string GetText (TextPoint start, TextPoint end)
     {
-      var start = _document.CreateEditPoint (_document.StartPoint);
-      return start.GetText (_document.EndPoint);
+      return _document.CreateEditPoint(start).GetText (end);
+    }
+
+    private string GetText()
+    {
+      return GetText (_document.StartPoint, _document.EndPoint);
     }
 
     private string GetExistingHeader ()
     {
-      string header = _parser.Parse (GetText ());
+      string header = _parser.Parse (GetText());
 
-      if (_keywords == null || _keywords.Any (k => header.ToLower ().Contains (k.ToLower ())))
+      if (_keywords == null || _keywords.Any (k => header.ToLower().Contains (k.ToLower())))
         return header;
       else
         return string.Empty;
@@ -43,11 +48,11 @@ namespace LicenseHeaderManager.Headers
 
     public void ReplaceHeaderIfNecessary ()
     {
-      var xml = GetXmlDeclaration ();
-      if (!string.IsNullOrEmpty(xml))
+      var xml = GetXmlDeclaration();
+      if (!string.IsNullOrEmpty (xml))
         RemoveHeader (xml);
 
-      string existingHeader = GetExistingHeader ();
+      string existingHeader = GetExistingHeader();
 
       if (_header != null)
       {
@@ -57,7 +62,7 @@ namespace LicenseHeaderManager.Headers
       else
         RemoveHeader (existingHeader);
 
-      if (!string.IsNullOrEmpty(xml))
+      if (!string.IsNullOrEmpty (xml))
         AddHeader (xml, false);
     }
 
@@ -67,7 +72,7 @@ namespace LicenseHeaderManager.Headers
       //Luckily we can use the same logic we use for finding comments to look for this
       //special tag and temporarily remove it so that the rest works as usual.
 
-      return new Parser (null, "<?", "?>", null, null).Parse (GetText ());
+      return new Parser (null, "<?", "?>", null, null).Parse (GetText());
 
       //Don't forget to add the tag again after modifying the file though!
     }
@@ -75,32 +80,60 @@ namespace LicenseHeaderManager.Headers
     private void ReplaceHeader (string existingHeader, string newHeader)
     {
       RemoveHeader (existingHeader);
-      AddHeader(newHeader);
+      AddHeader (newHeader);
     }
 
-    private void AddHeader (string header, bool appendEmptyLine = true)
+    private void AddHeader (string header, bool appendLineBreak = true, bool withEmptyLineIfNecessary = true)
     {
-      if (!string.IsNullOrEmpty(header))
-      {   
-        if (appendEmptyLine)
-          header += Environment.NewLine + Environment.NewLine;
+      if (!string.IsNullOrEmpty (header))
+      {
+        if (appendLineBreak)
+        {
+          header += Environment.NewLine;
+
+          if (withEmptyLineIfNecessary)
+          {
+            //if there's a comment right at the beginning of the file,
+            //we need to add an empty line so that the comment doesn't
+            //become a part of the header
+            if (!string.IsNullOrEmpty (_parser.Parse (GetText())))
+              header += Environment.NewLine;
+          }
+        }
 
         var start = _document.CreateEditPoint (_document.StartPoint);
         start.Insert (header);
       }
     }
 
+    private EditPoint EndOfHeader(string header, TextPoint start = null)
+    {
+      if (start == null)
+        start = _document.CreateEditPoint (_document.StartPoint);
+      var end = _document.CreateEditPoint (start);
+      end.CharRight (header.Length);
+
+      //CharRight always treats NewLines as single characters, so if that's not true in the current environment we need to take care of it
+      if (Environment.NewLine.Length > 1)
+      {
+        //count the NewLines in the header
+        int count = -1;
+        for (int index = 0; index >= 0; index = header.IndexOf (Environment.NewLine, index + Environment.NewLine.Length))
+          count++;
+
+        end.CharLeft (count * (Environment.NewLine.Length - 1));
+      }
+
+      return end;
+    }
+
     private void RemoveHeader (string header)
     {
       if (!string.IsNullOrEmpty (header))
-      { 
-        int count = -1;
-        for (int index = 0; index >= 0; index = header.IndexOf (Environment.NewLine, index + Environment.NewLine.Length))
-          count++;  
-
+      {
         var start = _document.CreateEditPoint (_document.StartPoint);
-        var end = _document.CreateEditPoint (_document.StartPoint);
-        end.CharRight (header.Length - count * (Environment.NewLine.Length - 1)); //otherwise too many characters would be deleted if NewLine's length is greater than 1
+        var end = EndOfHeader (header, _document.StartPoint);
+        
         start.Delete (end);
       }
     }
