c03806a47e4027d3a3ac6972e799e6065ddb8b3b
Fabian Schmied <fabian.schmied@rubicon.eu>
2011-12-19 11:56:12 +0100

    Moved workaround for embedded TextBox not accepting any characters from InteropTextBox to WpfHost - that's better than always having to think of using InteropTextBoxes Documented workaround Removed InteropTextBox

--- LicenseHeaderManager/Options/WpfHost.cs
+++ LicenseHeaderManager/Options/WpfHost.cs
@@ -14,15 +14,46 @@
 using System;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Interop;
 
 namespace LicenseHeaderManager.Options
 {
   public partial class WpfHost : UserControl
   {
+    private const UInt32 DLGC_WANTARROWS = 0x0001;
+    private const UInt32 DLGC_WANTTAB = 0x0002;
+    private const UInt32 DLGC_HASSETSEL = 0x0008;
+    private const UInt32 DLGC_WANTCHARS = 0x0080;
+    private const UInt32 WM_GETDLGCODE = 0x0087;
+
     public WpfHost (UIElement wpfControl)
     {
       InitializeComponent();
       elementHost.Child = wpfControl;
+    }
+
+    protected override void OnLoad (EventArgs e)
+    {
+      base.OnLoad (e);
+
+      // Workaround for an issue with WPF TextBox controls not working when hosted inside an ElementHost when the surrounding window swallows WM_CHAR 
+      // (and other) messages. We add a message hook to the HwndSource hosting the control inside the ElementHost that intercepts WM_GETDLGCODE to 
+      // specify that the control needs to handle WM_CHAR messages and similar.
+      // (Adapted from http://stackoverflow.com/questions/835878/wpf-textbox-not-accepting-input-when-in-elementhost-in-window-forms)
+
+      var hwndSource = PresentationSource.FromVisual (elementHost.Child) as HwndSource;
+      if (hwndSource != null)
+        hwndSource.AddHook (ChildHwndSourceHook);
+    }
+
+    private IntPtr ChildHwndSourceHook (IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+      if (msg == WM_GETDLGCODE)
+      {
+        handled = true;
+        return new IntPtr (DLGC_WANTCHARS | DLGC_WANTARROWS | DLGC_HASSETSEL | DLGC_WANTTAB);
+      }
+      return IntPtr.Zero;
     }
   }
 }
