e948f79a7f49b76bdbd4358a677aa595753e9934
Matthias Koch <matthias.koch@rubicon.eu>
2012-09-11 10:18:34 +0200

    - Added feature to auto-open header files after creation (#24)

--- LicenseHeaderManager/Headers/LicenseHeader.cs
+++ LicenseHeaderManager/Headers/LicenseHeader.cs
@@ -63,6 +63,8 @@ namespace LicenseHeaderManager.Headers
 
       if (newProjectItem != null)
       {
+        var window = newProjectItem.Open (Constants.vsViewKindCode);
+        window.Activate();
         return true;
       }
       else
