356ab71759d05a8bd7bbdea490cf1eb9589cec04
Matthias Koch <matthias.koch@rubicon.eu>
2012-10-08 11:08:11 +0200

    Incremented version

--- LicenseHeaderManager/LicenseHeadersPackage.cs
+++ LicenseHeaderManager/LicenseHeadersPackage.cs
@@ -76,7 +76,7 @@ namespace LicenseHeaderManager
     {
     }
 
-    public const string Version = "1.3.8";
+    public const string Version = "1.3.9";
 
     private const string c_licenseHeaders = "License Header Manager";
     private const string c_general = "General";
