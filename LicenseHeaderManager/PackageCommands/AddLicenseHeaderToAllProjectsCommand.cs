00ca6fb633338acdfc55e9bb6a3197fb763607db
Florian Decker <florian.decker@rubicon.eu>
2015-09-02 16:16:51 +0200

    Refactor addLicenseHeaderToAllProjectsCommand

--- LicenseHeaderManager/PackageCommands/AddLicenseHeaderToAllProjectsCommand.cs
+++ LicenseHeaderManager/PackageCommands/AddLicenseHeaderToAllProjectsCommand.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using EnvDTE;
-using EnvDTE80;
 using LicenseHeaderManager.Headers;
 using LicenseHeaderManager.Options;
 using LicenseHeaderManager.Utils;
@@ -15,68 +14,31 @@ namespace LicenseHeaderManager.PackageCommands
 {
   public class AddLicenseHeaderToAllProjectsCommand
   {
-    private LicenseHeadersPackage package;
     private LicenseHeaderReplacer licenseReplacer;
     private AddLicenseHeaderToAllFilesCommand addLicenseHeaderToAllFilesCommand;
     private IVsStatusbar statusBar;
+    private IDefaultLicenseHeaderPage licenseHeaderPage;
 
-    public AddLicenseHeaderToAllProjectsCommand(LicenseHeadersPackage package, IVsStatusbar statusbar)
+    public AddLicenseHeaderToAllProjectsCommand(LicenseHeaderReplacer licenseReplacer, IVsStatusbar statusBar, IDefaultLicenseHeaderPage licenseHeaderPage)
     {
-      this.package = package;
-      this.statusBar = statusbar;
-      
+      this.statusBar = statusBar;
+      this.licenseReplacer = licenseReplacer;
+      this.licenseHeaderPage = licenseHeaderPage;
+    
       addLicenseHeaderToAllFilesCommand = new AddLicenseHeaderToAllFilesCommand(licenseReplacer);
-      licenseReplacer = new LicenseHeaderReplacer (package);
-      
     }
 
     public void Execute(Solution solution)
     {
-      var projectsInSolution = new List<Project>();
+      var allSolutionProjectsSearcher = new AllSolutionProjectsSearcher();
+      var projectsInSolution = allSolutionProjectsSearcher.GetAllProjects(solution);
 
-      PopulateProjectsList(solution, projectsInSolution);
- 
       var projectsWithoutLicenseHeaderFile = CheckForLicenseHeaderFileInProjects (projectsInSolution);
 
       if (DefinitionFilesShouldBeAdded(projectsWithoutLicenseHeaderFile))
-        AddMissingLicenseHeaderFiles(projectsWithoutLicenseHeaderFile, package.DefaultLicenseHeaderPage);
+        AddMissingLicenseHeaderFiles(projectsWithoutLicenseHeaderFile, licenseHeaderPage);
     
       AddLicenseHeaderToProjects(projectsInSolution); 
-    }
-
-    private void PopulateProjectsList(Solution solution, List<Project> projectList)
-    {
-      foreach (Project project in solution)
-      {
-        if (project.Kind == ProjectKinds.vsProjectKindSolutionFolder)
-          projectList.AddRange(GetSolutionFolderProjects(project));
-        else
-          projectList.Add(project);
-      }
-    }
-
-    private IEnumerable<Project> GetSolutionFolderProjects(Project project)
-    {
-      List<Project> list = new List<Project> ();
-      for (var i = 1; i <= project.ProjectItems.Count; i++)
-      {
-        var subProject = project.ProjectItems.Item (i).SubProject;
-        if (subProject == null)
-        {
-          continue;
-        }
-
-        // If this is another solution folder, do a recursive call, otherwise add
-        if (subProject.Kind == ProjectKinds.vsProjectKindSolutionFolder)
-        {
-          list.AddRange (GetSolutionFolderProjects (subProject));
-        }
-        else
-        {
-          list.Add (subProject);
-        }
-      }
-      return list;
     }
 
     private List<Project> CheckForLicenseHeaderFileInProjects (List<Project> projects)
