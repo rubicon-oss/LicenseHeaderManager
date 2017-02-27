e8e1e5439dc89219ec842953542d84e4ff8cea56
Florian Decker <florian.decker@rubicon.eu>
2017-02-27 17:29:10 +0100

    Refactor the fix by seperating the ProjectItemParentFinder into its own class

--- LicenseHeaderManager/Headers/LicenseHeaderFinder.cs
+++ LicenseHeaderManager/Headers/LicenseHeaderFinder.cs
@@ -56,92 +56,10 @@ namespace LicenseHeaderManager.Headers
       if (!string.IsNullOrEmpty(headerFile))
         return LoadLicenseHeaderDefinition (headerFile); //Found a License header file on this level
       
-      var projectItemParent = GetProjectItemParent(projectItem);
+      var projectItemParent = ProjectItemParentFinder.GetProjectItemParent(projectItem);
 
       //Lookup in the parent --> Go Up!
       return GetHeaderRecursive (projectItemParent);
-    }
-
-    private static object GetProjectItemParent (ProjectItem projectItem)
-    {
-      object projectItemParent = null;
-
-      if (IsEndlessRecursion (projectItem))
-      {
-        projectItemParent = TryGetProjectIteamParentViaReflection (projectItem);
-
-        if (projectItemParent == null)
-        {
-          OutputWindowHandler.WriteMessage ($"Could not find LicenseHeader for {projectItem.Name}." +
-                                           "This is probably due to a custom project type." +
-                                           "Please report the issue and include the type of your project in the description.");
-        }
-      }
-      else
-      {
-        projectItemParent = projectItem.Collection.Parent;
-      }
-
-      return projectItemParent;
-    }
-
-    private static object TryGetProjectIteamParentViaReflection (ProjectItem projectItem)
-    {
-      try
-      {
-        if (projectItem.Object != null)
-        {
-          var parentProperty = projectItem.Object.GetType().GetProperty ("Parent").GetValue (projectItem.Object, null);
-          var parentUrl = parentProperty.GetType().GetProperty ("Url").GetValue (parentProperty, null) as string;
-          object projectItemParent = projectItem.DTE.Solution.FindProjectItem (parentUrl);
-
-          //If the ProjectItemParent could not be found by "FindProjectItem" this means we are a Folder at TopLevel and only the ContainingProject is above us
-          if (projectItemParent == null)
-          {
-            projectItemParent = projectItem.ContainingProject;
-          }
-
-          return projectItemParent;
-        }
-      }
-      catch (Exception exception)
-      {
-        //We catch everything as a multitude of Exceptions can be thrown if the projectItem.Object is not structured as we assume
-        OutputWindowHandler.WriteMessage (
-            string.Format (
-                "Exception got thrown when searching for the LicenseHeaderFile on FolderItem of Type '{0}' with the name '{1}'. Exception: {2}",
-                projectItem.GetType().FullName,
-                projectItem.Name,
-                exception));
-      }
-
-      return null;
-    }
-
-    private static bool IsEndlessRecursion (ProjectItem projectItem)
-    {
-      var projectItemPath = GetPath (projectItem);
-      var projectItemParent = projectItem.Collection.Parent as ProjectItem;
-
-      //ProjectItemParent is no ProjectItem --> 100% no endless recursion
-      if (projectItemParent == null)
-        return false;
-      
-      var parentProjectItemPath = GetPath (projectItemParent);
-
-      //If both paths are empty it is impossible to say if we are in an endless recursion
-      if (String.IsNullOrEmpty (projectItemPath) && String.IsNullOrEmpty (parentProjectItemPath))
-        return false;
-
-      //If the Paths are the same, we are in an endless recursion (projectItem.Parent == projectItem)
-      return projectItemPath == parentProjectItemPath;
-    }
-
-    private static string GetPath (ProjectItem projectItem)
-    {
-      var fullPathProperty = projectItem.Properties.Item ("FullPath");
-
-      return fullPathProperty?.Value.ToString() ?? String.Empty;
     }
 
     /// <summary>
