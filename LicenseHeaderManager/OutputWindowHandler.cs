264dfcdd856040c4953ea91dc13fc6c88a0aff85
Florian Decker <florian.decker@rubicon.eu>
2017-02-28 14:08:53 +0100

    Update OutputWindowHandler

--- LicenseHeaderManager/OutputWindowHandler.cs
+++ LicenseHeaderManager/OutputWindowHandler.cs
@@ -29,25 +29,25 @@ namespace LicenseHeaderManager
       _customGuid = new Guid(GuidList.guidVisualStudioOutputWindow);
     }
 
-    public static void WriteMessage(string message)
+    public static void WriteMessage (string message)
     {
       var customPane = GetOutputWindow();
 
       if (customPane == null)
       {
         string customTitle = Resources.NameOfThisExtension;
-        _outputWindow.CreatePane( ref _customGuid, customTitle, 1, 1 );
+        _outputWindow.CreatePane ( ref _customGuid, customTitle, 1, 1 );
         customPane = GetOutputWindow();
       }
       
-      customPane.OutputString(message);
+      customPane.OutputString (message + Environment.NewLine);
       customPane.Activate(); // Brings this pane into view
     }
 
     private static IVsOutputWindowPane GetOutputWindow()
     {
       IVsOutputWindowPane customPane;
-      _outputWindow.GetPane( ref _customGuid, out customPane);
+      _outputWindow.GetPane ( ref _customGuid, out customPane);
       return customPane;
     }
   }
