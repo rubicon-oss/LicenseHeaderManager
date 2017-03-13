aa62312d518771fbca1cd16d15cc7ca68d86b08a
Florian Decker <florian.decker@rubicon.eu>
2017-03-13 14:28:50 +0100

    The extra newline, which gets appended to our licenseheader in some cases, should be the same newline type as of the document

--- LicenseHeaderManager/Utils/LicenseHeaderPreparer.cs
+++ LicenseHeaderManager/Utils/LicenseHeaderPreparer.cs
@@ -23,17 +23,13 @@ namespace LicenseHeaderManager.Utils
     {
       var lineEndingInDocument = NewLineManager.DetectMostFrequentLineEnd (headerText);
 
-
-      var headerWithNewLine = headerText;
-      var newLine = NewLineManager.DetectMostFrequentLineEnd (headerWithNewLine);
-
       headerWithNewLine = NewLineManager.ReplaceAllLineEnds (headerWithNewLine, lineEndingInDocument);
       
       //if there's a comment right at the beginning of the file,
       //we need to add an empty line so that the comment doesn't
       //become a part of the header
       if (!string.IsNullOrEmpty (commentParser.Parse (currentHeaderText)) && !headerWithNewLine.EndsWith(lineEndingInDocument + lineEndingInDocument))
-        headerWithNewLine += newLine;
+        return headerWithNewLine + lineEndingInDocument;
       
 
       return headerWithNewLine;
