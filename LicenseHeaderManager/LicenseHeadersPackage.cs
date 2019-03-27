6f8afac2780cc6c3859623d06a93729de9e55f23
Florian Decker <florian.decker@rubicon.eu>
2019-03-27 15:04:47 +0100

    Bump version to 3.0.1

--- LicenseHeaderManager/LicenseHeadersPackage.cs
+++ LicenseHeaderManager/LicenseHeadersPackage.cs
@@ -79,7 +79,7 @@ namespace LicenseHeaderManager
     {
     }
 
-    public const string Version = "3.0.0";
+    public const string Version = "3.0.1";
 
     private const string c_licenseHeaders = "License Header Manager";
     private const string c_general = "General";
